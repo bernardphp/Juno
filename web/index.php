17d92653d57de56baa951abd7455996417a32619
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-18 15:01:33 +0100

    Move routing to Resources directory

--- web/index.php
+++ web/index.php
@@ -3,8 +3,8 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/henrikbjorn/raekke/vendor/autoload.php';
 
-$app = new Juno\Application(__DIR__ . '/../', true);
+$app = new Juno\Application($rootDir = __DIR__ . '/..', true);
 $app->inject(array(
-    'routing.resource' => 'config/routing.xml',
+    'routing.resource' => $rootDir . '/src/Juno/Resources/config/routing.xml',
 ));
 $app->run();
