12f57e6456637df935e2e61e6769b5537f2a6d17
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-19 09:04:15 +0100

    dont inject for a single value

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/henrikbjorn/raekke/vendor/autoload.php';
 
 $app = new Juno\Application($rootDir = __DIR__ . '/..', true);
-$app->inject(array(
-    'routing.resource' => $rootDir . '/src/Juno/Resources/config/routing.xml',
-));
+$app['routing.resource'] = $rootDir . '/src/Juno/Resources/config/routing.xml';
+
+// Run the thing
 $app->run();
