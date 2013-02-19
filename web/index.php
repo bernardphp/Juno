b311896739ab89adb7ca66a04b19f5a9c385373a
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-19 18:32:42 +0100

    add error handler and try and find the debug

--- web/index.php
+++ web/index.php
@@ -1,7 +1,18 @@
 <?php
 
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/henrikbjorn/raekke/vendor/autoload.php';
+
+ErrorHandler::register();
+
+$debug = in_array($_SERVER['REMOTE_ADDR'], array(
+    '127.0.0.1',
+    'fe80::1',
+    '::1',
+    '::ffff:127.0.0.1',
+));
 
 $app = new Juno\Application($rootDir = __DIR__ . '/..', true);
 $app->inject(array(
