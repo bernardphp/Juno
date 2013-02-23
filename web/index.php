41d7c824ac168bfbd66c50e5fdcab8c20fe88308
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-23 15:41:30 +0100

    simplify front controller

--- web/index.php
+++ web/index.php
@@ -1,21 +1,16 @@
 <?php
 
-use Symfony\Component\HttpKernel\Debug\ErrorHandler;
-
 require __DIR__ . '/../vendor/autoload.php';
 
-ErrorHandler::register();
-
-$debug = in_array($_SERVER['REMOTE_ADDR'], array(
-    '127.0.0.1',
-    'fe80::1',
-    '::1',
-    '::ffff:127.0.0.1',
-));
-
-$app = new Juno\Application($rootDir = __DIR__ . '/..', $debug);
+$app = new Juno\Application($rootDir = __DIR__ . '/..', false);
 $app->inject(array(
     'routing.resource' => $rootDir . '/src/Juno/Resources/config/routing.xml',
+    'routing.options' => array(
+        'cache_dir' => $rootDir . '/cache/routing',
+    ),
+    'twig.options' => array(
+        'cache' => $rootDir . '/cache/twig',
+    ),
     'predis.clients' => array(
         'raekke' => array(
             'parameters' => 'tcp://localhost',
