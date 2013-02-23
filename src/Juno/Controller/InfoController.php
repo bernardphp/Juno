b858a180e0e795507c10c253bed720d5c0ffdb7c
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-02-23 14:45:52 +0100

    normalize info

--- src/Juno/Controller/InfoController.php
+++ src/Juno/Controller/InfoController.php
@@ -28,15 +28,8 @@ class InfoController extends \Flint\Controller\Controller
      */
     public function connectionAction()
     {
-        // The connection should properly expose this information.
-        $redis = $this->app['predis']['raekke']->info();
-
-        uasort($redis, function ($a, $b) {
-            return count($b) - count($a);
-        });
-
         return $this->app['twig']->render('@Juno/Info/connection.html.twig', array(
-            'redis' => $redis,
+            'info' => $this->app['raekke.connection']->info(),
         ));
     }
 }
