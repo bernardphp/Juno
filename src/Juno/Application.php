55b328db80d5c56c879f2ccec8c0696adc0e0266
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-05-16 19:09:44 +0200

    Use Symfony Serializer instead of JMS

--- src/Juno/Application.php
+++ src/Juno/Application.php
@@ -2,10 +2,10 @@
 
 namespace Juno;
 
-use Juno\Provider\SerializerServiceProvider;
-use Juno\Provider\JunoServiceProvider;
 use Bernard\Silex\BernardServiceProvider;
+use Juno\Provider\JunoServiceProvider;
 use Predis\Silex\PredisServiceProvider;
+use Silex\Provider\SerializerServiceProvider;
 
 /**
  * @package Juno
@@ -18,6 +18,7 @@ class Application extends \Flint\Application
 
         $this->register(new PredisServiceProvider);
         $this->register(new BernardServiceProvider);
+        $this->register(new SerializerServiceProvider);
         $this->register(new JunoServiceProvider);
     }
 }
