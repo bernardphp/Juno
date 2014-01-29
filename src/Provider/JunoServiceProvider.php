20f004bbc98524801d6cb08b3cb1749daaa03c43
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-01-29 15:25:13 +0100

    consolidate providers

--- src/Provider/JunoServiceProvider.php
+++ src/Provider/JunoServiceProvider.php
@@ -3,15 +3,44 @@
 namespace Juno\Provider;
 
 use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Silex\ControllerProviderInterface;
 use Juno\EventListener\AcceptListener;
 
-class JunoServiceProvider implements \Silex\ServiceProviderInterface
+class JunoServiceProvider implements ServiceProviderInterface, ControllerProviderInterface
 {
     public function register(Application $app)
     {
+        $app['juno.mount_prefix'] = '/';
     }
 
     public function boot(Application $app)
     {
+        $app->mount($app['juno.mount_prefix'], $this->connect($app));
+    }
+
+    public function connect(Application $app)
+    {
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/queue.json', 'Juno\Controller\QueueController::indexAction')
+            ->bind('juno_queue_index');
+
+        $controllers->get('/queue/{queue}.json', 'Juno\Controller\QueueController::showAction')
+            ->bind('juno_queue_show');
+
+        $controllers->get('/consumer.json', 'Juno\Controller\ConsumerController::indexAction')
+            ->bind('juno_consumer_index');
+
+        $controllers->get('/info.json', 'Juno\Controller\InfoController::indexAction')
+            ->bind('juno_info_index');
+
+        $controllers->get('/info/driver.json', 'Juno\Controller\InfoController::driverAction')
+            ->bind('juno_info_driver');
+
+        $controllers->get('/{url}', 'Juno\Controller\DefaultController::indexAction')
+            ->bind('juno_homepage')->assert('url', '.{0,}?');
+
+        return $controllers;
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -3,18 +3,16 @@
 namespace Juno;
 
 use Bernard\Silex\BernardServiceProvider;
-use Juno\Provider\JunoControllerProvider;
 use Juno\Provider\JunoServiceProvider;
 use Silex\Application;
+use Silex\Provider\UrlGeneratorServiceProvider;
 
 function create_application($debug = false)
 {
     $app = new Application(compact('debug'));
-
+    $app->register(new UrlGeneratorServiceProvider);
     $app->register(new JunoServiceProvider);
     $app->register(new BernardServiceProvider);
-
-    $app->mount('/', new JunoControllerProvider);
 
     return $app;
 }
