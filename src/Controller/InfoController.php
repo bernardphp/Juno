b1ee3922ec499152cea8de882fd277d17ae61dd6
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-04 13:48:59 +0100

    add additional numbers under info

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -8,9 +8,17 @@ class InfoController
 {
     public function indexAction(Application $app)
     {
+        $driver = $app['bernard.driver'];
+        $queues = $driver->listQueues();
+        $failed = intval($driver->countMessages('failed'));
+        $messages = array_sum(array_map(array($driver, 'countMessages'), $queues));
+
         return $app->json(array(
-            'driver' => $app['bernard.driver']->info(),
-            'config' => $app['bernard.config'],
+            'queues'   => count($queues),
+            'messages' => $messages - $failed,
+            'failed'   => $failed,
+            'driver'   => $driver->info(),
+            'config'   => $app['bernard.config'],
         ));
     }
 }
