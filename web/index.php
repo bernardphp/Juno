1996c0423069a969bf64ea9197cbd66ffc5374d5
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-05 09:45:04 +0100

    use the same title for title tag and brand name

--- web/index.php
+++ web/index.php
@@ -5,4 +5,17 @@ namespace Juno;
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = create_application(true);
+$app->register(new \Silex\Provider\DoctrineServiceProvider);
+$app['dbs.options'] = array(
+    'bernard' => array(
+        'dbname' => 'bernard',
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'user' => 'root',
+        'oassword' => null,
+    )
+);
+$app['bernard.options'] = array(
+    'driver' => 'doctrine',
+);
 $app->run();
