4421efb400e9afa3729c82ef8b6450d3a8dbf268
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-06 09:57:39 +0100

    add sphinx_bernard_theme
    add favicon and less precise dependency
    
    remove faviocn
    
    requirements
    
    sphinx theme

--- doc/conf.py
+++ doc/conf.py
@@ -1,5 +1,11 @@
 # Build file for sphinx
 
+import sphinx_bernard_theme
+
+html_theme = "sphinx_bernard_theme"
+html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
+
 master_doc = 'index'
+
 project = u'Juno'
 copyright = u'MIT'
